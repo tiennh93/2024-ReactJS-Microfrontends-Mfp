a89e6e8d692411f667f8b4450666890f3393f4cb
Tien Nguyen <tien.nguyen4@ntq-solution.com.vn>
2024-12-18 08:57:00 +0700

    add Dashboard component

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -5,6 +5,7 @@ const packageJson = require('../package.json');
 
 const marketingDomain = process.env.PRODUCTION_MARKETING_DOMAIN; // config in Github
 const authDomain = process.env.PRODUCTION_AUTH_DOMAIN;
+const dashboardDomain = process.env.PRODUCTION_DASHBOARD_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
@@ -17,6 +18,7 @@ const prodConfig = {
       remotes: {
         marketing: `marketing@${marketingDomain}/remoteEntry.js`,
         auth: `auth@${authDomain}/remoteEntry.js`,
+        dashboard: `dashboard@${dashboardDomain}/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
